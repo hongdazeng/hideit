92666b3d6769197cc11028b23a7b18d33afc425f
Hongda Zeng <hongda.code@gmail.com>
2014-09-27 20:48:16 -0400

    added comments and randomString
    Added comments to describe the program
    Added the randomString() method which will generate a random string base
    on a preset group of characters.

--- Merger.java
+++ Merger.java
@@ -1,3 +1,13 @@
+/*
+ *Hide It 
+ * 
+ * This application will hide a string within another string by merging it with randomly generated string.
+ * 
+ * @Hongda Zeng (hongda.code@gmail.com)
+ * September 27, 2014
+ */
+import java.util.*;
+
 public class Merger {
     public static String merge (String a, String b) {
         String result = "";
@@ -5,5 +15,17 @@ public class Merger {
             result += "" + a.charAt(i) + b.charAt(i);
         }
         return result;
+    }
+    
+    public String randomString(int length){
+        final String Pickline = "0123456789abcdefghijklmnopqrestuvwxyz";
+        // The method will choose random characters from the above line
+        Random randGen = new Random();
+        // Create randomnese
+        StringBuilder builder = new StringBuilder(length);
+        for ( int i = 0; i < length; i++ ) {
+            builder.append ( Pickline.charAt(randGen.nextInt(Pickline.length())));
+        }
+        return builder.toString();
     }
 }
